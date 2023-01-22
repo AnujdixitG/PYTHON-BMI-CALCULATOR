92d09b5823958b9f74c91529e7625d2e2f1fbbee
Anuj Dixit <93566696+AnujdixitG@users.noreply.github.com>
2023-01-22 16:57:05 +0530

    Add files via upload

---
+++ bmi.py
@@ -0,0 +1,11 @@
+#crdt_anuj  BMI CALCULATER
+
+hei = input('Enter your height in Metres: ')
+
+wei = input('Enter your weight in kgs: ')
+
+bmi_fo = (float(hei))**2 / int(wei) #or we can do hei * hei
+
+bmi_whole = int(bmi_fo)
+
+print(bmi_whole)
